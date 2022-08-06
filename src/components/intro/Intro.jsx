e9c053ba81599ba85ba860b5b275b4403ad088ff
Ben Hessel <hesselfitness@gmail.com>
2022-08-06 18:21:30 -0400

    save, may need to rollback

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './intro.scss';
+import React from "react";
+import "./intro.scss";
+import Man from "../../images/man.png"
 
 const Intro = () => {
-    return (
-        <div className='intro' id='intro'>
-            asdf
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src={Man} alt=""></img>
         </div>
-    );
-}
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
 
 export default Intro;
