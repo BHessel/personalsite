e5d00a282807948ccb961a3903bea897699064c0
Ben Hessel <hesselfitness@gmail.com>
2022-08-07 09:51:34 -0400

    able to scroll thru/select top bar options now

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import './menu.scss';
+import React from "react";
+import "./menu.scss";
 
 const Menu = (props) => {
-    
-    const { burgerOpen, setBurgerOpen } = props
+  const { burgerOpen, setBurgerOpen } = props;
 
-    const menuItems = ["Home", "Portfolio", "Works", "Testimonials", "Contact"];
-    
-    return (
-        <div className={"menu" + `${burgerOpen ? " active" : ""}`}>
-            <ul>
-                {menuItems.map((item, index) => {
-                    return (
-                        <li 
-                            key={index}
-                            onClick={() => setBurgerOpen(!burgerOpen)}
-                        >
-                            <a href={`#${item.toLowerCase()}`}>{item}</a>
-                        </li>
-                    )
-                })}
-            </ul>
-            
-        </div>
-    );
-}
+  const menuItems = ["Home", "Portfolio", "Works", "Testimonials", "Contact"];
+
+  return (
+    <div className={"menu" + `${burgerOpen ? " active" : ""}`}>
+      <ul>
+        {menuItems.map((item, index) => {
+          return (
+            <li key={index} onClick={() => setBurgerOpen(!burgerOpen)}>
+              <a href={`#${item.toLowerCase()}`}>{item}</a>
+            </li>
+          );
+        })}
+      </ul>
+      
+      
+    </div>
+  );
+};
 
 export default Menu;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import './portfolio.scss';
+import React, { useState } from "react";
+import "./portfolio.scss";
+import NetflixMatcher from "../../images/netflixmmlogo.png";
 
 const Portfolio = () => {
-    return (
-        <div className='portfolio' id='portfolio'>
-            asdfasdf
+  const [active, setActive] = useState("Featured");
+
+  const portfolioItems = [
+    "Featured",
+    "Web App",
+    "Mobile App",
+    "Design",
+    "Branding",
+  ];
+
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {portfolioItems.map((item, index) => {
+          return (
+            <li
+              key={index}
+              className={
+                (active.toLowerCase() === item.toLowerCase()) 
+                  ? "portfolioList active"
+                  : "porfolioList"
+              }
+              onClick={() => setActive(item)}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="container">
+        <div className="item">
+          <img src={NetflixMatcher} alt="" />
+          <h3>Netflix Movie Matcher</h3>
         </div>
-    );
-}
+        <div className="item">
+          <img src={NetflixMatcher} alt="" />
+          <h3>Netflix Movie Matcher</h3>
+        </div>
+        <div className="item">
+          <img src={NetflixMatcher} alt="" />
+          <h3>Netflix Movie Matcher</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
