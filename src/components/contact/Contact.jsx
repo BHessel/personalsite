04ad59e7df51de1afd5d4e8648396bc3ef40a179
Ben Hessel <hesselfitness@gmail.com>
2022-08-07 16:40:11 -0400

    contact form/page done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import './contact.scss'
+import React, { useState } from "react";
+import "./contact.scss";
+import shake from "../../images/shake.svg";
 
 const Contact = () => {
-    return (
-        <div className='contact' id='contact'>
-            contact
-        </div>
-    );
-}
+
+    const [ message, setMessage ] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true)
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src={shake} alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" placeholder="Email"></input>
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message ? (
+            <span>Thanks for the message, I look forward to meeting you!</span>
+          ) : (
+            ""
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
