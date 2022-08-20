8222cada9d278ad62b6917569e6f8cff0b8804fc
Ben Hessel <hesselfitness@gmail.com>
2022-08-20 12:58:21 -0400

    diff views coming through in portfolio, next replace w/ my data, re-assess

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,54 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./portfolio.scss";
-import NetflixMatcher from "../../images/netflixmmlogo.png";
-import PortfolioData from './PortfolioData'
+import { WebAppData, BlogData } from "./PortfolioData";
+import {
+  featuredPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolo,
+  webPortfolio,
+} from "../../data";
 
 const Portfolio = () => {
-  const [active, setActive] = useState("Featured");
+  const [active, setActive] = useState("featured");
+  const [view, setView] = useState([]);
 
   const portfolioItems = [
-    "Featured",
-    "Web App",
-    "Blogs",
-    "Current Areas of Study"
+    { id: "featured", title: "Featured" },
+    { id: "web", title: "Web Apps" },
+    { id: "mobile", title: "Blogs" },
+    { id: "design", title: "Current Areas of Study" },
+    { id: "content", title: "Current Areas of Study" },
   ];
 
+  useEffect(() => {
+    switch (active) {
+      case "featured":
+        setView(featuredPortfolio);
+        break;
+      case "web":
+        setView(webPortfolio);
+        break;
+      case "mobile":
+        setView(mobilePortfolio);
+        break;
+      case "design":
+        setView(designPortfolio);
+        break;
+      default:
+        setView(featuredPortfolio);
+    }
+  }, [active]);
 
+
+  console.log(portfolioItems);
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        {portfolioItems.map((item, index) => {
-          return (
-            <li
-              key={index}
-              className={
-                (active.toLowerCase() === item.toLowerCase()) 
-                  ? "portfolioList active"
-                  : "porfolioList"
-              }
-              onClick={() => setActive(item)}
-            >
-              {item}
-            </li>
-          );
-        })}
+        {portfolioItems.map((item, index) => (
+          <li
+            key={index}
+            className={
+              active === item.id ? "portfolioList active" : "porfolioList"
+            }
+            onClick={() => setActive(item.id)}
+          >
+            {item.title}
+          </li>
+        ))}
       </ul>
 
       <div className="container">
-        <div className="item">
-          <img src={NetflixMatcher} alt="" />
-          <h3>Netflix Movie Matcher</h3>
+        {view.map((viewItem, index) => (
+          <div key={index} className="item">
+            <img src={viewItem.img} alt="" />
+            <h3>{viewItem.title}</h3>
+          </div>
+        ))}
         </div>
-        <div className="item">
-          <img src={NetflixMatcher} alt="" />
-          <h3>Netflix Movie Matcher</h3>
-        </div>
-        <div className="item">
-          <img src={NetflixMatcher} alt="" />
-          <h3>Netflix Movie Matcher</h3>
-        </div>
-      </div>
     </div>
   );
 };
--- src/components/portfolio/PortfolioData.jsx
+++ src/components/portfolio/PortfolioData.jsx
@@ -1,34 +1,49 @@
 import NetflixLogo from "../../images/netflixmmlogo.png";
 import LASC from "../../images/LASC.png";
 import Bowser from "../../images/bowser.png";
+import Thinking from "../../images/thinking.jpeg";
+import How from '../../images/how.png'
+import Grid from '../../images/grid.jpeg'
 
-export const WebApp = [
+export const WebAppData = [
   {
     id: 1,
     title: "Netflix Movie Matcher",
     img: { NetflixLogo },
-    link: "" ,
+    link: "https://netflixmoviematcher.herokuapp.com/" ,
   },
   {
     id: 2,
     title: "Lonely Alien Todo List",
     img: { LASC },
-    link: "",
-  },
-  {
-    id: 3,
-    title: "E-commerce Web Design",
-    img: "asdf",
-    link: "",
+    link: "https://alientodolist.herokuapp.com/",
   },
 ];
 
-export const Blogs = [
+export const BlogData = [
     {
         id: 1,
         title: "An Intro to Test-Driven Development and Jest",
         img: { Bowser },
-        link: "",
+        link: "https://benlearnstocode.medium.com/an-intro-test-driven-development-and-jest-b934704664bc",
+    },
+    {
+        id: 2,
+        title: "useContext for Global State",
+        img: { Thinking },
+        link: "https://benlearnstocode.medium.com/usecontext-for-global-state-9d45c40316af",
+    },
+    {
+        id: 3,
+        title: "React Virtual DOM 101",
+        img: { How },
+        link: "https://benlearnstocode.medium.com/react-virtual-dom-101-fa374e29f624",
+    },
+    {
+        id: 4,
+        title: "An Intro to CSS Grid",
+        img: { Grid },
+        link: "https://benlearnstocode.medium.com/an-intro-to-css-grid-595f19fa7f75",
     }
 ];
 
